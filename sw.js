18ad447b61e1a8c5eb861ed8963b8577cad8cd57
XUEYAN CHEN <sheryyan727@gmail.com>
2026-09-18 16:11:36 +0800

    Publish urticaria record app

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE = "urticaria-v5";
+const ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
+self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
+self.addEventListener("fetch", event => event.respondWith(fetch(event.request).then(response => {
+  if (response.ok) caches.open(CACHE).then(cache => cache.put(event.request, response.clone()));
+  return response;
+}).catch(() => caches.match(event.request))));
