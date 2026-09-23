c1cc94aeaea6e71fa178c4042c52e7b4159a6540
XUEYAN CHEN <sheryyan727@gmail.com>
2026-09-23 10:03:27 +0800

    Refresh cached app files promptly

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
-const CACHE = "urticaria-v16";
+const CACHE = "urticaria-v17";
 const ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
-self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
+self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener("activate", event => event.waitUntil(clients.claim()));
 self.addEventListener("fetch", event => event.respondWith(fetch(event.request).then(response => {
   if (response.ok) caches.open(CACHE).then(cache => cache.put(event.request, response.clone()));
   return response;
